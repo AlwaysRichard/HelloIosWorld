f4e93d1b326a295ebad9caec848d03d3e85c6c3f
AlwaysRichard <AlwaysRichard@gmail.com>
2014-10-12 22:06:29 -0400

    Initial Commit

---
+++ HelloIosWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloIosWorld
+//
+//  Created by Richard Cox on 10/12/14.
+//  Copyright (c) 2014 Richard Cox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloIosWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloIosWorld
+//
+//  Created by Richard Cox on 10/12/14.
+//  Copyright (c) 2014 Richard Cox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
